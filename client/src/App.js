a0b675f7988b803af873fb3cce92f568e7563d0a
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2023-03-19 23:12:28 +0700

    Amazon_List_Products

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import data from "./data";
+import Data from "./data";
 
 function App() {
   return (
@@ -6,7 +8,26 @@ function App() {
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
