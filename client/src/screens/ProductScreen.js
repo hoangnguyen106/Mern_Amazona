8fe0642a04442e0657d5a1f4ec14bdb4e4510842
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2023-03-21 21:26:06 +0700

    Amazon_Create_Cart_Screen

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 import Row from "react-bootstrap/esm/Row";
 import ListGroup from "react-bootstrap/ListGroup";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import logger from "use-reducer-logger";
 import Rating from "../components/Rating";
 import Button from "react-bootstrap/esm/Button";
@@ -29,6 +29,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(logger(reducer), {
@@ -64,6 +65,7 @@ function ProductScreen() {
       type: "CART_ADD_ITEM",
       payload: { ...product, quantity },
     });
+    navigate("/cart");
   };
 
   return loading ? (
