b2867f08bbec3d34c7528416b7447e506ca5e97e
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2023-03-23 22:17:00 +0700

    Amazon_Create_Place_Order_Screen

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import { NavDropdown } from "react-bootstrap";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import SignupScreen from "./screens/SignupScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -90,6 +91,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/" element={<HomeScreen />} />
